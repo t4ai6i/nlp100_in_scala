3acf8736b52fe9e22aebc3d6327742098dfc8c30
t4ai6i <t4ai6i>
2016-11-05 01:41:15 +0900

    Add nlp section1 04

--- src/test/scala/NLPSection1Spec.scala
+++ src/test/scala/NLPSection1Spec.scala
@@ -4,6 +4,8 @@
 
 import org.specs2.mutable._
 
+import scala.collection.immutable.TreeMap
+
 class NLPSection1Spec extends Specification {
   "NLP 100 section1" >> {
     "文字列\"stressed\"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．" >> {
@@ -30,6 +32,18 @@ class NLPSection1Spec extends Specification {
       val answer = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
         .split(" ").map(_.count(_.isLetter)).mkString
       answer must_== "314159265358979"
+    }
+
+    """"Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+      |という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，
+      |取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型）を作成せよ．""".stripMargin >> {
+      val headOnes = Seq(1, 5, 6, 7, 8, 9, 15, 16, 19).map(_ - 1)
+      val answer = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+        .split(" ").zipWithIndex.collect {
+        case (w, i) if headOnes.exists(_ == i) => w.substring(0, 1) -> i
+        case (w, i) => w.substring(0, 2) -> i
+      }.toMap
+      answer must_== Map("Cl" -> 16, "N" -> 6, "Be" -> 3, "F" -> 8, "Mi" -> 11, "Na" -> 10, "Ne" -> 9, "Ar" -> 17, "B" -> 4, "Li" -> 2, "P" -> 14, "He" -> 1, "Si" -> 13, "C" -> 5, "H" -> 0, "Ca" -> 19, "K" -> 18, "Al" -> 12, "O" -> 7, "S" -> 15)
     }
   }
 }
