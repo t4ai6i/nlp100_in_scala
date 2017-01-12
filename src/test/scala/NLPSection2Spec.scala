5f4ed1f4edd1d79eff50fd32d5a67b9750dfd102
t4ai6i <t4ai6i>
2017-01-12 21:42:46 +0900

    Improve to treat file lines

--- src/test/scala/NLPSection2Spec.scala
+++ src/test/scala/NLPSection2Spec.scala
@@ -5,19 +5,35 @@
 import java.io.File
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.commons.io.FileUtils
+import org.apache.commons.io.{FileUtils, LineIterator}
 import org.specs2.mutable._
-import scala.collection.JavaConversions._
+import java.nio.charset.{Charset, StandardCharsets}
+
+import scala.collection.JavaConverters._
+import scala.util.control.Exception._
 
 class NLPSection2Spec extends Specification with LazyLogging {
 
+  def open[X](file: File, charset: Charset = StandardCharsets.UTF_8)(invalid: Throwable => X, body: Iterator[String] => X) = {
+    val lineIterator = FileUtils.lineIterator(file, charset.toString)
+    allCatch withApply { t =>
+      invalid(t)
+    } andFinally {
+      LineIterator.closeQuietly(lineIterator)
+    } apply {
+      body(lineIterator.asScala.map(_.toString))
+    }
+  }
+
   private val filePath = "src/test/resources/hightemp.txt"
   private val file = new File(filePath)
-  private val lines = FileUtils.readLines(file)
 
   "NLP 100 section2" >> {
     "10. 行数のカウント" >> {
-      val answer = lines.length
+      val answer = open(file)(
+        th => 0,
+        ite => ite.length
+      )
       answer must_== 24
     }
   }
