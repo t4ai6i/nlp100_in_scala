c117dd79574cbab7b6f321ee64c0e341b62a863b
t4ai6i <t4ai6i>
2017-01-13 23:53:30 +0900

    #open throws exception

--- src/test/scala/NLPSection2Spec.scala
+++ src/test/scala/NLPSection2Spec.scala
@@ -14,10 +14,10 @@ import scala.util.control.Exception._
 
 class NLPSection2Spec extends Specification with LazyLogging {
 
-  def open[X](file: File, charset: Charset = StandardCharsets.UTF_8)(invalid: Throwable => X, body: Iterator[String] => X) = {
+  def open[X](file: File, charset: Charset = StandardCharsets.UTF_8)(body: Iterator[String] => X) = {
     val lineIterator = FileUtils.lineIterator(file, charset.toString)
     allCatch withApply { t =>
-      invalid(t)
+      throw t
     } andFinally {
       LineIterator.closeQuietly(lineIterator)
     } apply {
@@ -25,47 +25,48 @@ class NLPSection2Spec extends Specification with LazyLogging {
     }
   }
 
-  private def splitColumn(file: File, errorHandler: Throwable => Unit = th => ()) = open(file)(
-    th => {
-      errorHandler(th)
-      (Vector.empty[String], Vector.empty[String])
-    },
-    ite => {
-      val split = ite.map(_.split("\t").take(2))
-      val (xs, ys) = split.duplicate
-      val col1 = xs.map(_ (0)).toVector
-      val col2 = ys.map(_ (1)).toVector
-      (col1, col2)
-    }
-  )
-
   private val filePath = "src/test/resources/hightemp.txt"
   private val file = new File(filePath)
 
   "NLP 100 section2" >> {
     "10. 行数のカウント" >> {
-      val answer = open(file)(
-        th => 0,
-        ite => ite.length
-      )
+      val answer = allCatch withApply { th =>
+        logger.error("error", th)
+        0
+      } apply {
+        open(file) { ite =>
+          ite.length
+        }
+      }
       answer must_== 24
     }
     "11. タブをスペースに置換" >> {
-      val answer = open(file)(
-        th => {
-          logger.error("error", th)
-          Vector.empty[String]
-        },
-        ite => {
-          ite.map(_.replaceAll("\t", " ")).toVector
+      val xs = allCatch withApply { th =>
+        logger.error("error", th)
+        Vector.empty[String]
+      } apply {
+        open(file) { ite =>
+            ite.map(_.replaceAll("\t", " ")).toVector
         }
-      ).head
-      answer must_== "高知県 江川崎 41 2013-08-12"
+      }
+      val answer = xs.headOption
+      answer must_== Some("高知県 江川崎 41 2013-08-12")
     }
     "12. 1列目をcol1.txtに，2列目をcol2.txtに保存" >> {
-      val split = splitColumn(file)
-      val answer = (split._1 zip split._2).head
-      answer must_== (("高知県", "江川崎"): (String, String))
+      val split = allCatch withApply { th =>
+        logger.error("error", th)
+        (Vector.empty[String], Vector.empty[String])
+      } apply {
+        open(file) { ite =>
+          val split = ite.map(_.split("\t").take(2))
+          val (xs, ys) = split.duplicate
+          val col1 = xs.map(_ (0)).toVector
+          val col2 = ys.map(_ (1)).toVector
+          (col1, col2)
+        }
+      }
+      val answer = (split._1 zip split._2).headOption
+      answer must_== Some(("高知県", "江川崎"))
     }
   }
 }
