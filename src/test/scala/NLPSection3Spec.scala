ceeaf9c2f7ce56358601f9d8545d45cf46bb3386
t4ai6i <t4ai6i>
2017-02-09 22:28:47 +0900

    Add nlp section3 23

--- src/test/scala/NLPSection3Spec.scala
+++ src/test/scala/NLPSection3Spec.scala
@@ -105,6 +105,32 @@ class NLPSection3Spec extends Specification with LazyLogging {
         "イギリス|*", "英連邦王国|*", "G8加盟国", "欧州連合加盟国", "海洋国家", "君主国",
         "島国|くれいとふりてん", "1801年に設立された州・地域")
     }
+
+    "23. セクション構造" >> {
+      val answer = file2iterator(engFile) { ite =>
+        val r = """^(=+)\s*(.*?)\s*(=+)$""".r
+        val (articles, _) = validatedArticles(ite)
+        val sections = for {
+          article <- articles
+          lines = article.text.split(lineSeparator)
+          line <- lines
+          matched = r.findFirstMatchIn(line)
+          matched <- matched
+          name = matched.group(2)
+          level = matched.group(1).length
+        } yield {
+          (name, level)
+        }
+        sections.toVector
+      }
+      answer must_== Vector(
+        ("国名", 2), ("歴史", 2), ("地理", 2), ("気候", 3), ("政治", 2), ("外交と軍事", 2), ("地方行政区分", 2),
+        ("主要都市", 3), ("科学技術", 2), ("経済", 2), ("鉱業", 3), ("農業", 3), ("貿易", 3), ("通貨", 3),
+        ("企業", 3), ("交通", 2), ("道路", 3), ("鉄道", 3), ("海運", 3), ("航空", 3), ("通信", 2), ("国民", 2),
+        ("言語", 3), ("宗教", 3), ("婚姻", 3), ("教育", 3), ("文化", 2), ("食文化", 3), ("文学", 3), ("哲学", 3),
+        ("音楽", 3), ("イギリスのポピュラー音楽", 4), ("映画", 3), ("コメディ", 3), ("国花", 3), ("世界遺産", 3),
+        ("祝祭日", 3), ("スポーツ", 2), ("サッカー", 3), ("競馬", 3), ("モータースポーツ", 3), ("脚注", 2),
+        ("関連項目", 2), ("外部リンク", 2))
     }
 
     "24. ファイル参照の抽出" >> {
