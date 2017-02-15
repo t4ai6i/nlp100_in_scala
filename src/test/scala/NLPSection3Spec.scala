4a572778b20ab7f8761f7a4fe2ba9baadfb95830
t4ai6i <t4ai6i>
2017-02-15 15:44:49 +0900

    Add nlp section3 24

--- src/test/scala/NLPSection3Spec.scala
+++ src/test/scala/NLPSection3Spec.scala
@@ -100,5 +100,58 @@ class NLPSection3Spec extends Specification with LazyLogging {
       }
       answer must_== Vector("イギリス|*", "英連邦王国|*", "G8加盟国", "欧州連合加盟国", "海洋国家", "君主国", "島国|くれいとふりてん", "1801年に設立された州・地域")
     }
+
+    "24. ファイル参照の抽出" >> {
+      val answer = file2iterator(engFile) { ite =>
+        val r = """(File|ファイル):(.*?)\|""".r
+        val (articles, _) = validatedArticles(ite)
+        val names = for {
+          article <- articles
+          lines = article.text.split(lineSeparator)
+          line <- lines
+          matched = r.findFirstMatchIn(line)
+          matched <- matched
+          name = matched.group(2)
+        } yield {
+          name
+        }
+        names.toVector
+      }
+      answer must_== Vector(
+        "Royal Coat of Arms of the United Kingdom.svg",
+        "Battle of Waterloo 1815.PNG",
+        "The British Empire.png",
+        "Uk topo en.jpg",
+        "BenNevis2005.jpg",
+        "Elizabeth II greets NASA GSFC employees, May 8, 2007 edit.jpg",
+        "Palace of Westminster, London - Feb 2007.jpg",
+        "David Cameron and Barack Obama at the G20 Summit in Toronto.jpg",
+        "Soldiers Trooping the Colour, 16th June 2007.jpg",
+        "Scotland Parliament Holyrood.jpg",
+        "London.bankofengland.arp.jpg",
+        "City of London skyline from London City Hall - Oct 2008.jpg",
+        "Oil platform in the North SeaPros.jpg",
+        "Eurostar at St Pancras Jan 2008.jpg",
+        "Heathrow T5.jpg",
+        "Anglospeak.svg",
+        "CHANDOS3.jpg",
+        "The Fabs.JPG",
+        "PalaceOfWestminsterAtNight.jpg",
+        "Westminster Abbey - West Door.jpg",
+        "Edinburgh Cockburn St dsc06789.jpg",
+        "Canterbury Cathedral - Portal Nave Cross-spire.jpeg",
+        "Kew Gardens Palm House, London - July 2009.jpg",
+        "2005-06-27 - United Kingdom - England - London - Greenwich.jpg",
+        "Stonehenge2007 07 30.jpg",
+        "Yard2.jpg",
+        "Durham Kathedrale Nahaufnahme.jpg",
+        "Roman Baths in Bath Spa, England - July 2006.jpg",
+        "Fountains Abbey view02 2005-08-27.jpg",
+        "Blenheim Palace IMG 3673.JPG",
+        "Liverpool Pier Head by night.jpg",
+        "Hadrian's Wall view near Greenhead.jpg",
+        "London Tower (1).JPG",
+        "Wembley Stadium, illuminated.jpg")
+    }
   }
 }
