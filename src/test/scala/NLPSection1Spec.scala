8d0fcf9d7f401d950c076bc3335492fc46120010
t4ai6i <t4ai6i>
2017-10-20 10:16:54 +0900

    Implove 04

--- src/test/scala/NLPSection1Spec.scala
+++ src/test/scala/NLPSection1Spec.scala
@@ -116,13 +116,40 @@ class NLPSection1Spec extends Specification with LazyLogging {
     """04."Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
       |という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，
       |取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型）を作成せよ．""".stripMargin >> {
-      val headOnes = Seq(1, 5, 6, 7, 8, 9, 15, 16, 19).map(_ - 1)
-      val answer = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-        .split(" ").zipWithIndex.collect {
-        case (w, i) if headOnes.exists(_ == i) => w.substring(0, 1) -> i
-        case (w, i) => w.substring(0, 2) -> i
-      }.toMap
-      answer must_== Map("Cl" -> 16, "N" -> 6, "Be" -> 3, "F" -> 8, "Mi" -> 11, "Na" -> 10, "Ne" -> 9, "Ar" -> 17, "B" -> 4, "Li" -> 2, "P" -> 14, "He" -> 1, "Si" -> 13, "C" -> 5, "H" -> 0, "Ca" -> 19, "K" -> 18, "Al" -> 12, "O" -> 7, "S" -> 15)
+      val expected = Map(16 -> "Cl", 6 -> "N", 3 -> "Be", 8 -> "F", 11 -> "Mi", 10 -> "Na", 9 -> "Ne", 17 -> "Ar", 4 -> "B", 2 -> "Li", 14 -> "P", 1 -> "He", 13 -> "Si", 5 -> "C", 0 -> "H", 19 -> "Ca", 18 -> "K", 12 -> "Al", 7 -> "O", 15 -> "S")
+      val string = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+      val headOne = Seq(1, 5, 6, 7, 8, 9, 15, 16, 19).map(_ - 1)
+
+      def containInHeadOne(number: Int): Int = number match {
+        case n if headOne.contains(n) => 1
+        case _ => 2
+      }
+
+      "#zipWithIndex" >> {
+        val answer = string.split("""\W+""").zipWithIndex.map {
+          case (w, i) => i -> w.take(containInHeadOne(i))
+        }.toMap
+        answer must_== expected
+      }
+      "#foldLeft" >> {
+        val (_, answer) = string.split("""\W+""").foldLeft((0, Map.empty[Int, String])) {
+          case ((current, map), word) =>
+            (current + 1, map.updated(current, word.take(containInHeadOne(current))))
+        }
+        answer must_== expected
+      }
+      "Regex" >> {
+        var n: Int = 0
+        val poss = "^(1|5|6|7|8|9|15|16|19)$".r
+        val answer = string.split("""\W+""").map { word =>
+          n += 1
+          n.toString match {
+            case poss(_) => (n - 1, word.take(1))
+            case _ => (n - 1, word.take(2))
+          }
+        }.toMap
+        answer must_== expected
+      }
     }
 
     """05.与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
