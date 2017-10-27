1a492caa3f990ca939755373b61f7e3c8b07fee8
t4ai6i <t4ai6i>
2017-10-27 11:53:19 +0900

    Remove unused val

--- src/test/scala/NLPSection1Spec.scala
+++ src/test/scala/NLPSection1Spec.scala
@@ -18,7 +18,6 @@ class NLPSection1Spec extends Specification with LazyLogging {
     case c => c
   }.mkString
 
-  var expected: String = _
   "NLP 100 section1" >> {
     "00.文字列\"stressed\"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．" >> {
       val answer = "stressed".reverse
